3f3486be7013f9a10f2b58062a7861ef2607f16c
ajinkya2020 <ajinkyachamp20@gmail.com>
2020-10-02 13:39:25 +0530

    Reset Password and User Profile.

--- app/src/main/java/com/example/esportsarena/Login.java
+++ app/src/main/java/com/example/esportsarena/Login.java
@@ -1,17 +1,30 @@
 package com.example.esportsarena;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView register;
+    private TextView register, mForgotPassword;
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,19 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.loginButton);
+        signIn.setOnClickListener(this);
+
+        mForgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        mForgotPassword.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.emailID);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar2);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -28,6 +54,58 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             case R.id.register:
                 startActivity(new Intent(this, Register.class));
                 break;
+
+            case R.id.loginButton:
+                userLogin();
+                break;
+
+            case R.id.forgotPassword:
+                startActivity(new Intent(this, ForgotPassword.class));
+                break;
         }
+    }
+
+    private void userLogin()
+    {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            editTextEmail.setError("Email-ID is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            editTextEmail.setError("Email-ID invalid!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                    progressBar.setVisibility(View.GONE);
+                }
+                else
+                {
+                    Toast.makeText(Login.this, "Login failed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/esportsarena/MainActivity.java
+++ app/src/main/java/com/example/esportsarena/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.esportsarena;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,26 +8,67 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    private Button login;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button mlogoutButton;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = (Button) findViewById(R.id.loginButton);
-        login.setOnClickListener(this);
-    }
+        mlogoutButton = (Button) findViewById(R.id.logoutButton);
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.loginButton:
-                startActivity(new Intent(this, Login.class));
-                break;
-        }
+        mlogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
+        final TextView userNameTextView = (TextView) findViewById(R.id.userName);
+        final TextView emailIDTextView = (TextView) findViewById(R.id.emailAddress);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null)
+                {
+                    String userName = userProfile.userName;
+                    String email = userProfile.email;
+                    greetingTextView.setText("Welcome, " + userName + "!");
+                    userNameTextView.setText(userName);
+                    emailIDTextView.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "An error occurred!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
